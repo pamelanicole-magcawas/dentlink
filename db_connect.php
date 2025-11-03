514762497d6f42fa8b3146ed4e0cc443107f942d
Angela Gizelle Dayo <dayoangelagizelle@gmail.com>
2025-11-03 13:23:25 +0800

    credentials

--- db_connect.php
+++ db_connect.php
@@ -1,11 +1,25 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "dental_clinic"; // change to your DB name
+class Database {
+    private $host = "localhost";     
+    private $db_name = "dental_clinic"; 
+    private $username = "root";       
+    private $password = "";           
+    private $conn;
 
-$conn = new mysqli($host, $user, $pass, $db);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    public function getConnect() {
+        if ($this->conn == null) {
+            try {
+                $this->conn = new PDO(
+                    "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                    $this->username,
+                    $this->password
+                );
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                die("Database connection error: " . $e->getMessage());
+            }
+        }
+        return $this->conn;
+    }
 }
 ?>
